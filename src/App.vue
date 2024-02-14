1f2ee77d15d75920d32b0edd346fbb4e3da36be3
GloriaBimbi <gloria.bimbi1@libero.it>
2024-02-14 14:00:48 +0100

    feat MILESTONE 0

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<!-- 5cf2112ddf23b3b71a19a74fcd05ae67 -->
+
+<script>
+import store from "./store/index";
+
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+};
+</script>
+
+<template></template>
+
+<style lang="scss">
+@use "./styles/general.scss" as *;
+</style>
