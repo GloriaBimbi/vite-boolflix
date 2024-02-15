de69c7033d426e3e1dc798e7af98c4e8003e63c9
GloriaBimbi <gloria.bimbi1@libero.it>
2024-02-15 13:35:37 +0100

    fix store.results

--- src/App.vue
+++ src/App.vue
@@ -10,17 +10,33 @@ export default {
     };
   },
   methods: {
-    searchMovieTitle() {
+    fetchTitle() {
       axios
         .get(store.apiUriMovies + store.movieTitle)
         .then((response) => {
-          store.results = response.data;
-          console.log(response.data);
+          store.results = response.data.results.map((result) => {
+            return {
+              title: result.title,
+              original_title: result.original_title,
+              language: result.original_language,
+              vote: result.vote_average,
+            };
+          });
+          console.log(response.data.results);
         })
         .catch((error) => {
           alert(error.message);
         });
       store.movieTitle = "";
+    },
+    searchMovieTitle() {
+      if (store.movieTitle != "") {
+        this.fetchTitle();
+      } else {
+        alert(
+          "ERRORE: per avviare la ricerca devi inserire almeno un carattere"
+        );
+      }
     },
     // searchTvSerieTitle() {
     //   axios
@@ -35,49 +51,49 @@ export default {
     //   store.movieTitle = "";
     // },
     generateFlagImg(i) {
-      if (store.results.results[i].original_language == "ja") {
+      if (store.results[i].language == "ja") {
         return "/img/ja-flag.jpg";
       }
-      if (store.results.results[i].original_language == "en") {
+      if (store.results[i].language == "en") {
         return "/img/en-flag.jpg";
       }
-      if (store.results.results[i].original_language == "it") {
+      if (store.results[i].language == "it") {
         return "/img/it-flag.jpg";
       }
-      if (store.results.results[i].original_language == "fr") {
+      if (store.results[i].language == "fr") {
         return "/img/fr-flag.jpg";
       }
-      if (store.results.results[i].original_language == "de") {
+      if (store.results[i].language == "de") {
         return "/img/de-flag.jpg";
       }
-      if (store.results.results[i].original_language == "pt") {
+      if (store.results[i].language == "pt") {
         return "/img/pt-flag.jpg";
       }
-      if (store.results.results[i].original_language == "ru") {
+      if (store.results[i].language == "ru") {
         return "/img/ru-flag.jpg";
       }
-      if (store.results.results[i].original_language == "fi") {
+      if (store.results[i].language == "fi") {
         return "/img/fi-flag.jpg";
       }
-      if (store.results.results[i].original_language == "da") {
+      if (store.results[i].language == "da") {
         return "/img/da-flag.jpg";
       }
-      if (store.results.results[i].original_language == "sr") {
+      if (store.results[i].language == "sr") {
         return "/img/sr-flag.jpg";
       }
-      if (store.results.results[i].original_language == "ko") {
+      if (store.results[i].language == "ko") {
         return "/img/ko-flag.jpg";
       }
-      if (store.results.results[i].original_language == "hi") {
+      if (store.results[i].language == "hi") {
         return "/img/hi-flag.jpg";
       }
-      if (store.results.results[i].original_language == "bn") {
+      if (store.results[i].language == "bn") {
         return "/img/bn-flag.jpg";
       }
-      if (store.results.results[i].original_language == "no") {
+      if (store.results[i].language == "no") {
         return "/img/no-flag.jpg";
       }
-      if (store.results.results[i].original_language == "es") {
+      if (store.results[i].language == "es") {
         return "/img/es-flag.jpg";
       }
 
@@ -107,7 +123,7 @@ export default {
         Search
       </button>
     </div>
-    <ol v-for="(result, index) in store.results.results">
+    <ol v-for="(result, index) in store.results">
       <li>Titolo: {{ result.title }}</li>
       <li>Titolo Originale: {{ result.original_title }}</li>
       <li>
@@ -117,9 +133,8 @@ export default {
           alt="flag image"
           style="max-width: 30px"
         />
-        {{ result.original_language }}
       </li>
-      <li>Voto: {{ result.vote_average }}/10</li>
+      <li>Voto: {{ result.vote }}/10</li>
     </ol>
   </div>
 </template>
