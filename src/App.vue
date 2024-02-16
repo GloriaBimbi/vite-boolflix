a8b41b80e26a34d88afa3959538bbe9b48efbd49
GloriaBimbi <gloria.bimbi1@libero.it>
2024-02-16 12:43:25 +0100

    feat AppHeader

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script>
 import AppMain from "./components/AppMain.vue";
+import AppHeader from "./components/AppHeader.vue";
+
 import { store } from "./store/index";
 import axios from "axios";
 export default {
@@ -8,12 +10,15 @@ export default {
       store,
     };
   },
-  components: { AppMain },
+  components: { AppMain, AppHeader },
 };
 </script>
 
 <template>
-  <app-main></app-main>
+  <div class="container">
+    <app-header></app-header>
+    <app-main></app-main>
+  </div>
 </template>
 
 <style lang="scss">
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,84 @@
+<script>
+import { store } from "../store/index";
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+  methods: {
+    fetchMovieTitle() {
+      axios
+        .get(store.apiUriMovies + store.movieTitle)
+        .then((response) => {
+          store.resultMovies = response.data.results.map((result) => {
+            return {
+              title: result.title,
+              original_title: result.original_title,
+              language: result.original_language,
+              vote: result.vote_average,
+              poster_path: result.poster_path,
+              id: result.id,
+            };
+          });
+          console.log(response.data.results);
+        })
+        .catch((error) => {
+          alert(error.message);
+        });
+    },
+    fetchTvSerieTitle() {
+      axios
+        .get(store.apiUriTvSeries + store.movieTitle)
+        .then((response) => {
+          store.resultTvSeries = response.data.results.map((result) => {
+            return {
+              title: result.name,
+              original_title: result.original_name,
+              language: result.original_language,
+              vote: result.vote_average,
+              poster_path: result.poster_path,
+              id: result.id,
+            };
+          });
+          console.log(response.data.results);
+        })
+        .catch((error) => {
+          alert(error.message);
+        });
+    },
+    searchTitle() {
+      if (store.movieTitle != "") {
+        this.fetchMovieTitle();
+        this.fetchTvSerieTitle();
+      } else {
+        alert(
+          "ERRORE: per avviare la ricerca devi inserire almeno un carattere"
+        );
+      }
+      store.movieTitle = "";
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="col-auto">
+    <input
+      @keyup.enter="searchTitle()"
+      v-model="store.movieTitle"
+      type="text"
+      class="form-control"
+      placeholder="Harry Potter"
+    />
+  </div>
+  <div class="col-auto">
+    <button @click="searchTitle()" type="submit" class="btn btn-primary mb-3">
+      Search
+    </button>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
